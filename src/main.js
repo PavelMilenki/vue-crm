a79a3f6185a7d07706768572d33302fa341106e6
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-03-19 05:53:04 +0300

    added layout, login and register

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+import Vuelidate from 'vuelidate'
 import router from './router'
 import store from './store'
+import dateFilter from './filters/date.filter'
+import messagePlugin from './utils/message.plagin'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
