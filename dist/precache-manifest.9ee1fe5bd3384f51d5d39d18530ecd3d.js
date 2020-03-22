25cc8b7999536d0997f511361dccb2a74371daf5
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-03-22 04:29:24 +0300

    added build

---
+++ dist/precache-manifest.9ee1fe5bd3384f51d5d39d18530ecd3d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca80b3aec834cabcffbe",
+    "url": "/css/app.34323f0d.css"
+  },
+  {
+    "revision": "52e08c73de44a07283f1",
+    "url": "/css/chunk-vendors.2955f2db.css"
+  },
+  {
+    "revision": "a62c9cb69b325fc340f6df824c085724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca80b3aec834cabcffbe",
+    "url": "/js/app.94939558.js"
+  },
+  {
+    "revision": "6af8b2acec42bbec77c5",
+    "url": "/js/chunk-10b86dd9.fb58b024.js"
+  },
+  {
+    "revision": "8df856f1e7ef6873b0ee",
+    "url": "/js/chunk-2d217a99.43cf99c4.js"
+  },
+  {
+    "revision": "0cbc6c94ae61c23ceda0",
+    "url": "/js/chunk-2d21a3d2.2514276e.js"
+  },
+  {
+    "revision": "9c2a0db1a7f6447ab108",
+    "url": "/js/chunk-49ee4951.1877b0d1.js"
+  },
+  {
+    "revision": "5141bfc1c14d1f394f89",
+    "url": "/js/chunk-53c8ec65.78c94f56.js"
+  },
+  {
+    "revision": "45e9ed03ebd11bb01338",
+    "url": "/js/chunk-5f4ad635.736baf22.js"
+  },
+  {
+    "revision": "484e535f9ce17a8ab6e9",
+    "url": "/js/chunk-6ecc2d95.9e82c78d.js"
+  },
+  {
+    "revision": "6d01ef4a1edf73bd0929",
+    "url": "/js/chunk-72449522.1ea26012.js"
+  },
+  {
+    "revision": "e39de8ef65c85ebd4c64",
+    "url": "/js/chunk-c420df12.af7d66fa.js"
+  },
+  {
+    "revision": "52e08c73de44a07283f1",
+    "url": "/js/chunk-vendors.6be554cb.js"
+  },
+  {
+    "revision": "6a8d2c9e18169fece64b94593566de0d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
